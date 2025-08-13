4617d1741cf84d483074ceb7f6bb0d7df36a6b39
Igor Shalyminov <shalymin@amazon.com>
2025-08-13 04:16:08 +0000

    fix

--- verifiers/envs/environment.py
+++ verifiers/envs/environment.py
@@ -796,6 +796,7 @@ Model copies with swapped templates are available here: https://huggingface.co/c
             if max_seq_len > 0 and len(prompt_ids) + len(completion_ids) > max_seq_len:
                 if len(prompt_ids) > max_seq_len:
                     prompt_ids = prompt_ids[:max_seq_len]
+                    prompt_mask = prompt_mask[:max_seq_len]
                 completion_ids = completion_ids[: max_seq_len - len(prompt_ids)]
                 completion_mask = completion_mask[: max_seq_len - len(prompt_ids)]
                 completion_logprobs = completion_logprobs[
